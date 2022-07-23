242ff0252359130595dd12d3f61c419ff671c80f
QuantumGoddess <amber.vanraak@gmail.com>
2022-07-23 02:41:10 +0200

    added comments

--- src/main/java/net/quantumgoddess/oxygencraft/mixin/LivingEntityMixin.java
+++ src/main/java/net/quantumgoddess/oxygencraft/mixin/LivingEntityMixin.java
@@ -6,10 +6,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 
 @Mixin(LivingEntity.class)
@@ -24,22 +26,26 @@ public class LivingEntityMixin {
     @Inject(at = @At("HEAD"), method = "baseTick()V")
     public void baseTick(CallbackInfo info){
         LivingEntity entity = ((LivingEntity)(Object)this);
+        //copied drowning code and changed to work in air
         if (entity.isAlive()) {
             boolean bl = entity instanceof PlayerEntity;
-            //if (entity.world.getBlockState(new BlockPos(entity.getX(), entity.getEyeY(), entity.getZ())).isOf(Blocks.AIR)) {
-                boolean bl2;
-                boolean bl3 = bl2 = (!bl || !((PlayerEntity)entity).getAbilities().invulnerable);
+            //check if player is in air
+            if (entity.world.getBlockState(new BlockPos(entity.getX(), entity.getEyeY(), entity.getZ())).isOf(Blocks.AIR)) {
+                //check if entity is a player or is invulnerable
+                boolean bl2 = (!bl || !((PlayerEntity)entity).getAbilities().invulnerable);
                 if (bl2) {
                     entity.setAir(getNextAir(entity, entity.getAir()));
                     if (entity.getAir() == -20) {
                         entity.setAir(0);
+                        //TODO: set damage source?
                         entity.damage(DamageSource.DROWN, 2.0f);
                     }
                 }
-           // }
+            }
         }
     }
 
+    //calculate amount of air left on next tick
     protected int getNextAir(LivingEntity ent, int air) {
         int i = EnchantmentHelper.getRespiration(ent);
         if (i > 0 && ((LivingEntityAccessor) ent).getRandom().nextInt(i + 1) > 0) {
