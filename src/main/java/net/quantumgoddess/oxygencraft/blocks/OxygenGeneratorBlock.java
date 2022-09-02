5a005e6b9f3507b21334a4fbc6735ec25073b4a1
QuantumGoddess <amber.vanraak@gmail.com>
2022-09-02 20:56:41 +0200

    created oxygen generator block

---
+++ src/main/java/net/quantumgoddess/oxygencraft/blocks/OxygenGeneratorBlock.java
@@ -0,0 +1,12 @@
+package net.quantumgoddess.oxygencraft.blocks;
+
+import net.minecraft.block.Block;
+
+public class OxygenGeneratorBlock extends Block{
+
+    public OxygenGeneratorBlock(Settings settings) {
+        super(settings);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
